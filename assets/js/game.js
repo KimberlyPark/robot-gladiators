dd2668461e92d5cf9e5fa5a1d7cf45f22434a397
Kimberly Park <quorrbbeeflash@yahoo.com>
2021-09-29 12:37:57 -0600

    another commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,27 @@
-//  user input
-var playerName = window.prompt("What is your robot's name?");
-console.log(playerName)
-// capture data - robots name 'var playerName = '
+// this creates a function named "fight"
 
+//window.prompt("What is your robot's name?"); // -1
+var playerName = window.prompt("What is your robot's name?");
+//^captures the data, user driven
+
+// Note the lack of quotation marks around playerName -2 shows smuffy in browser
+//window.alert(playerName);// smuffy
+
+console.log(playerName);
 console.log("This logs a string, good for leaving yourself a message");
 // this will do math and log 20
 console.log(10 + 10);
 // what is this?
 console.log("Our robot's name is " + playerName);
+var name = "your name";
+console.log(name)
 
 
-// this creates a function named "fight"
 function fight() {
-    window.alert("The fight has begun!");
-  }
+  window.alert("The fight has begun!");
+}
+
+
+
 
 // fight();
